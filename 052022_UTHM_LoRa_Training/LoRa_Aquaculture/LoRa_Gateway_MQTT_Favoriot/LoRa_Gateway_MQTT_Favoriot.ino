7d287426966286f51b83511a414f0534f86f315c
ariffinzulkifli <ariffinastute@live.com>
2022-05-08 18:22:52 +0800

    update sketch

--- 052022_UTHM_LoRa_Training/LoRa_Aquaculture/LoRa_Gateway_MQTT_Favoriot/LoRa_Gateway_MQTT_Favoriot.ino
+++ 052022_UTHM_LoRa_Training/LoRa_Aquaculture/LoRa_Gateway_MQTT_Favoriot/LoRa_Gateway_MQTT_Favoriot.ino
@@ -1,15 +1,49 @@
 #include <SPI.h>
 #include <LoRa.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+#define OLED_RESET     4
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup() {
   Serial.begin(9600);
 
-  Serial.println("LoRa Receiver");
+  Serial.println();
+  Serial.print("Initialize LoRa Gateway: ");
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) Serial.println(F("SSD1306 allocation failed"));
+
+  display.clearDisplay();
+
+  // display.setRotation(2);
+  
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE, SSD1306_BLACK);
+  display.setCursor(0, 0);
+  display.print("Myinvent LoRa: ");
+  display.display();
+
+  SPI.begin(5, 19, 27, 18);
+  LoRa.setPins(18, 23, 26);
 
   if (!LoRa.begin(923E6)) {
-    Serial.println("Initializing LoRa failed!");
+    Serial.println("NOT OK");
+    display.println("NOT OK");
+    display.display();
     while (1);
   }
+
+  Serial.println("OK");
+  display.print("OK");
+
+  display.setCursor(0, 20);
+  display.print("Received Packet:");
+  display.display();
+  
 }
 
 void loop() {
@@ -23,12 +57,18 @@ void loop() {
     // read packet
     String packet;
     while (LoRa.available()) {
-      packet += LoRa.read();
+      packet += (char)LoRa.read();
     }
-    Serual.print(packet);
-
+    Serial.print(packet);
+    
     // print RSSI of packet
     Serial.print("' with RSSI ");
     Serial.println(LoRa.packetRssi());
+
+    display.setCursor(0, 30);
+    display.println("> " + packet);
+
+    display.print("> RSSI: " + String(LoRa.packetRssi())  + " dBm");
+    display.display();
   }
 }
