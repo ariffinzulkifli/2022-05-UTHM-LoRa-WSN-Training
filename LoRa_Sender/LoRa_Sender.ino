06ce7a890ee4c2999606a82258fb429c9db751f7
ariffinzulkifli <ariffinastute@live.com>
2022-05-08 18:31:57 +0800

    update sketch

--- LoRa_Sender/LoRa_Sender.ino
+++ LoRa_Sender/LoRa_Sender.ino
@@ -1,73 +1,55 @@
 #include <SPI.h>
-#include <RH_RF95.h>
- 
-#define RFM95_CS 10
-#define RFM95_RST 7
-#define RFM95_INT 2
-#define node_id "B"
- 
-// Change to 434.0 or other frequency, must match RX's freq!
-#define RF95_FREQ 923.0
- 
-// Singleton instance of the radio driver
-RH_RF95 rf95(RFM95_CS, RFM95_INT);
- 
-void setup() {
-  pinMode(RFM95_RST, OUTPUT);
-  digitalWrite(RFM95_RST, HIGH);
- 
-  while (!Serial);
-  Serial.begin(9600);
-  delay(100);
- 
-  Serial.println("Arduino LoRa TX Test!");
- 
-  // manual reset
-  digitalWrite(RFM95_RST, LOW);
-  delay(10);
-  digitalWrite(RFM95_RST, HIGH);
-  delay(10);
- 
-  while (!rf95.init()) {
-    Serial.println("LoRa radio init failed");
-    while (1);
-  }
-  
-  Serial.println("LoRa radio init OK!");
- 
-  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
-  if (!rf95.setFrequency(RF95_FREQ)) {
-    Serial.println("setFrequency failed");
-    while (1);
-  }
-  
-  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
-  
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
- 
-  // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
-  // you can set transmitter powers from 5 to 23 dBm:
-  rf95.setTxPower(23, false);
-}
- 
-int16_t packetnum = 0;  // packet counter, we increment per xmission
- 
-void loop() {
-  Serial.println("Sending to rf95_server");
-  // Send a message to rf95_server
-  
-  String radiopacket = "Hello World #";
-  radiopacket += String(packetnum++);
-  radiopacket += " from ";
-  radiopacket += node_id;
-  
-  Serial.print("Sending "); 
-  Serial.println(radiopacket); delay(10);
-  rf95.send((uint8_t*)(radiopacket.c_str()), radiopacket.length()+1);
+#include <LoRa.h>
 
-  Serial.println("Waiting for packet to complete..."); delay(10);
-  rf95.waitPacketSent();
+int counter = 0;
+
+// LoRa Module Pin Definition
+#define SPI_MOSI            13
+#define SPI_MISO            12
+#define SPI_SCK             14
+
+#define LORA_RST            33
+#define LORA_CS             32
+
+#define LORA_DIO0           25
+#define LORA_DIO1           27
+
+#define LORA_FREQUENCY      923.0
+#define LORA_BANDWIDTH      125.0
+#define LORA_SPREADING      7
+#define LORA_CODING_RATE    7
+#define LORA_OUTPUT_POWER   10
+#define LORA_PREAMBLE_LEN   8
+#define LORA_GAIN           0
+
+void setup() {
+  Serial.begin(9600);
+
+  Serial.println("LoRa Sender");
+
+  SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI, LORA_CS);
+  LoRa.setPins(LORA_CS, LORA_RST, LORA_DIO0);
   
-  delay(5000); 
+  if (!LoRa.begin(LORA_FREQUENCY)) {
+    Serial.println("> Starting LoRa failed!");
+    while(1);
+  }
+
+  // LoRa.setSyncWord(LORA_SYNC_WORD); // default 0x34
+  LoRa.setSpreadingFactor(LORA_SPREADING);
+}
+
+void loop() {
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+
+  // send packet
+  LoRa.beginPacket();
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  counter++;
+
+  delay(5000);
 }
